0d4a61180ce5b14cb529fb7113b374f39973980b
jonaykb <jonaykb@gmail.com>
2024-10-16 12:08:43 +0100

    feat(Api): Added api base structure

---
+++ api/routinefights/src/main/java/es/ies/puerto/routinefights/RoutinefightsApplication.java
@@ -0,0 +1,13 @@
+package es.ies.puerto.routinefights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinefightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutinefightsApplication.class, args);
+	}
+
+}
---
+++ api/routinefights/src/test/java/es/ies/puerto/routinefights/RoutinefightsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ies.puerto.routinefights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinefightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
