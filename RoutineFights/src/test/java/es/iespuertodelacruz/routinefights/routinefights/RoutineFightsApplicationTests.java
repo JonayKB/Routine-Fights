d44f1844ecf936755ca82c3ab45fbca2f71b1777
JonayKB <jonaykb@gmail.com>
2025-02-09 01:02:48 +0000

    test(Application): Fixed not assetrion test

--- RoutineFights/src/test/java/es/iespuertodelacruz/routinefights/routinefights/RoutineFightsApplicationTests.java
+++ RoutineFights/src/test/java/es/iespuertodelacruz/routinefights/routinefights/RoutineFightsApplicationTests.java
@@ -1,14 +1,16 @@
 package es.iespuertodelacruz.routinefights.routinefights;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoutineFightsApplicationTests {
 
-	@Test
-    void testMain() {
-		RoutineFightsApplication.main(new String[] {});
-    }
+  @Test
+  void testMain() {
+    assertDoesNotThrow(() -> RoutineFightsApplication.main(new String[] {}));
+  }
 
 }
