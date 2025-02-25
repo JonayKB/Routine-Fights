a7d6476d3759b3b0aeb6d0375bf7e680234189e0
JonayKB <jonaykb@gmail.com>
2025-02-25 10:04:45 +0000

    feat(JwtService): Added jwtSecurty and tests for it
    32

--- API/src/main/java/es/iespuertodelacruz/routinefights/RoutineFightsApplication.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/RoutineFightsApplication.java
@@ -1,4 +1,4 @@
-package es.iespuertodelacruz.routinefights.routinefights;
+package es.iespuertodelacruz.routinefights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
