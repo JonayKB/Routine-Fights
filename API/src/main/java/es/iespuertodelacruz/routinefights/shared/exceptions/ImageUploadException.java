46814f4e861c35f9c583b15e7631af5637888c66
JonayKB <jonaykb@gmail.com>
2025-02-28 22:21:14 +0000

    feat(Images): :sparkles: Created images manager (Service and controller)
    Closes #43, #44

---
+++ API/src/main/java/es/iespuertodelacruz/routinefights/shared/exceptions/ImageUploadException.java
@@ -0,0 +1,8 @@
+package es.iespuertodelacruz.routinefights.shared.exceptions;
+
+public class ImageUploadException extends RuntimeException {
+    public ImageUploadException(String message) {
+        super(message);
+    }
+
+}
