b1647f6697ab3fa4828781b06b07086996d4c776
rabgonzalez <rubalba.rag@gmail.com>
2025-03-02 17:27:53 +0000

    test(UserEntityMapperTest): mapper tests added

--- API/src/main/java/es/iespuertodelacruz/routinefights/shared/security/JwtFilter.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/shared/security/JwtFilter.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
--- API/src/main/java/es/iespuertodelacruz/routinefights/shared/services/MailService.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/shared/services/MailService.java
@@ -1,6 +1,5 @@
 package es.iespuertodelacruz.routinefights.shared.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
