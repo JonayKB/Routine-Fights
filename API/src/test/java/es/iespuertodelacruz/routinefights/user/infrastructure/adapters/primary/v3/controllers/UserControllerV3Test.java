81509e836d241487dc61019575bd0e8e80c5503b
rabgonzalez <rubalba.rag@gmail.com>
2025-03-02 19:35:39 +0000

    test(UserEntityServiceTest): userEntityService tests added

---
+++ API/src/test/java/es/iespuertodelacruz/routinefights/user/infrastructure/adapters/primary/v3/controllers/UserControllerV3Test.java
@@ -0,0 +1,75 @@
+package es.iespuertodelacruz.routinefights.user.infrastructure.adapters.primary.v3.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerV3Test {
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindAllImages() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+    }
+
+    @Test
+    void testFindFollowedUsersByEmail() {
+
+    }
+
+    @Test
+    void testFindFollowersByEmail() {
+
+    }
+
+    @Test
+    void testFollowUser() {
+
+    }
+
+    @Test
+    void testGetUserOutputMapper() {
+
+    }
+
+    @Test
+    void testGetUserService() {
+
+    }
+
+    @Test
+    void testPost() {
+
+    }
+
+    @Test
+    void testPut() {
+
+    }
+
+    @Test
+    void testSetUserOutputMapper() {
+
+    }
+
+    @Test
+    void testSetUserService() {
+
+    }
+
+    @Test
+    void testUnfollowUser() {
+
+    }
+}
