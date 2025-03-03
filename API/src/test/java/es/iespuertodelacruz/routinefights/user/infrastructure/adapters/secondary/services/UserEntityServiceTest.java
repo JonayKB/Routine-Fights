8c1c5d0b8e6a30e5a396e15aabf3db677a9325c6
rabgonzalez <rubalba.rag@gmail.com>
2025-03-03 13:36:09 +0000

    test(UserEntityTest): some exceptions covered

--- API/src/test/java/es/iespuertodelacruz/routinefights/user/infrastructure/adapters/secondary/services/UserEntityServiceTest.java
+++ API/src/test/java/es/iespuertodelacruz/routinefights/user/infrastructure/adapters/secondary/services/UserEntityServiceTest.java
@@ -1,6 +1,8 @@
 package es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -16,6 +18,9 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.exceptions.UserNotFoundException;
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.exceptions.UserSaveException;
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.exceptions.UserUpdateException;
 import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.entities.UserEntity;
 import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.mappers.IUserEntityMapper;
 import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.repositories.IUserEntityRepository;
@@ -27,7 +32,7 @@ class UserEntityServiceTest extends UserInitializer {
 
     @Mock
     private IUserEntityRepository userEntityRepository;
-    
+
     @Mock
     private PasswordEncoder passwordEncoder;
 
@@ -75,9 +80,29 @@ class UserEntityServiceTest extends UserInitializer {
     }
 
     @Test
+    void findAllExceptionTest() {
+        when(userEntityRepository.findAll()).thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.findAll();
+        });
+        assertEquals("Users not found", exception.getMessage());
+    }
+
+    @Test
     void findAllImagesTest() {
         when(userEntityRepository.findAllImages()).thenReturn(new ArrayList<String>());
         assertNotNull(userEntityService.findAllImages());
+    }
+
+    @Test
+    void findAllImagesExceptionTest() {
+        when(userEntityRepository.findAllImages()).thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.findAllImages();
+        });
+        assertEquals("Error finding images", exception.getMessage());
     }
 
     @Test
@@ -95,9 +120,30 @@ class UserEntityServiceTest extends UserInitializer {
     }
 
     @Test
+    void findByIdExceptionTest() {
+        when(userEntityRepository.findById(anyString())).thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.findById("id");
+        });
+        assertEquals("User not found", exception.getMessage());
+    }
+
+    @Test
     void findFollowedUsersByEmailTest() {
         when(userEntityRepository.findFollowedUsersByEmail(anyString())).thenReturn(new ArrayList<UserEntity>());
         assertNotNull(userEntityService.findFollowedUsersByEmail("email"));
+    }
+
+    @Test
+    void findFollowedUsersByEmailExceptionTest() {
+        when(userEntityRepository.findFollowedUsersByEmail(anyString()))
+                .thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.findFollowedUsersByEmail("email");
+        });
+        assertEquals("Followed users not found", exception.getMessage());
     }
 
     @Test
@@ -107,10 +153,32 @@ class UserEntityServiceTest extends UserInitializer {
     }
 
     @Test
+    void findFollowersByEmailExceptionTest() {
+        when(userEntityRepository.findFollowersByEmail(anyString()))
+                .thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.findFollowersByEmail("email");
+        });
+        assertEquals("Followers not found", exception.getMessage());
+    }
+
+    @Test
     void followUserTest() {
         when(userEntityRepository.followUser(anyString(), anyString())).thenReturn(true);
         Boolean followed = userEntityService.followUser("frEmail", "fdEmail");
         assertTrue(followed);
+    }
+
+    @Test
+    void followUserExceptionTest() {
+        when(userEntityRepository.followUser(anyString(), anyString()))
+                .thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.followUser("frEmail", "fdEmail");
+        });
+        assertEquals("Error following user", exception.getMessage());
     }
 
     @Test
@@ -122,6 +190,16 @@ class UserEntityServiceTest extends UserInitializer {
     }
 
     @Test
+    void postExceptionTest() {
+        when(userEntityRepository.save(any(UserEntity.class))).thenThrow(new UserSaveException("Test Exception"));
+
+        UserSaveException exception = assertThrows(UserSaveException.class, () -> {
+            userEntityService.post(user);
+        });
+        assertEquals("Error saving user", exception.getMessage());
+    }
+
+    @Test
     void putTest() {
         when(userEntityRepository.save(any(UserEntity.class))).thenReturn(new UserEntity());
         when(userEntityRepository.findById(anyString())).thenReturn(Optional.of(userEntity));
@@ -130,9 +208,29 @@ class UserEntityServiceTest extends UserInitializer {
     }
 
     @Test
+    void putExceptionTest() {
+        when(userEntityRepository.save(any(UserEntity.class))).thenThrow(new UserUpdateException("Test Exception"));
+
+        UserUpdateException exception = assertThrows(UserUpdateException.class, () -> {
+            userEntityService.put(user);
+        });
+        assertEquals("Unable to update the user", exception.getMessage());
+    }
+
+    @Test
     void unfollowUserTest() {
         when(userEntityRepository.unfollowUser(anyString(), anyString())).thenReturn(true);
         Boolean unfollowed = userEntityService.unfollowUser("frEmail", "fdEmail");
         assertTrue(unfollowed);
+    }
+
+    @Test
+    void unfollowUserExceptionTest() {
+        when(userEntityRepository.unfollowUser(anyString(), anyString())).thenThrow(new UserNotFoundException("Test Exception"));
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userEntityService.unfollowUser("frEmail", "fdEmail");
+        });
+        assertEquals("Error unfollowing user", exception.getMessage());
     }
 }
