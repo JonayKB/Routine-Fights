1b046cfe9b53ca2c9c3237df1aa79ef2f72121f2
rabgonzalez <rubalba.rag@gmail.com>
2025-03-12 12:23:05 +0100

    feat(App): Login Navigation started, custom font added

--- App/App.tsx
+++ App/App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginNavigation from './src/navigation/LoginNavigation';
-import './global.css'
+import './global.css';
 
 function App(): React.JSX.Element {
   return (
