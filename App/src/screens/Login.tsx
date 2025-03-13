28602ef53d049774c10ae7b6920f52eeac099a0d
rabgonzalez <rubalba.rag@gmail.com>
2025-03-13 18:38:58 +0100

    styles(Register): register screen finished

--- App/src/screens/Login.tsx
+++ App/src/screens/Login.tsx
@@ -6,8 +6,9 @@ import { UserIn } from "../utils/User";
 
 type Props = NativeStackScreenProps<LoginStackProps, "Login">;
 
-const Login = ({ navigation, route }: Props) => {
+const Login = ({ navigation }: Props) => {
   const [userIn, setUserIn] = useState<UserIn>({} as UserIn);
+  const [passwordShown, setPasswordShown] = useState<boolean>(false);
 
   const login = () => {
     Alert.alert("Login", JSON.stringify(userIn));
@@ -25,25 +26,33 @@ const Login = ({ navigation, route }: Props) => {
     <View className="flex-1 bg-[#E4DCE9] justify-center items-center">
       <View
         className="justify-evenly bg-white rounded-2xl w-96"
-        style={{ height: 413 }}
+        style={{ height: 400 }}
       >
-        <View className="m-10">
+        <View className="m-10 mb-5">
           <TextInput
             placeholder="Email"
             placeholderTextColor="#4B0082"
             inputMode="email"
-            className="border-[#4B0082] border-2 rounded-lg bg-[#F8F7FE] text-2xl mb-5 pl-3"
+            className="border-[#4B0082] border-2 rounded-lg bg-[#F8F7FE] text-2xl mb-5 pl-3 text-black"
             onChangeText={(text) => setUserIn({ ...userIn, email: text })}
           />
           <TextInput
             placeholder="Password"
             placeholderTextColor="#4B0082"
-            secureTextEntry={true}
-            className="border-[#4B0082] border-2 rounded-lg bg-[#F8F7FE] text-2xl pl-3"
+            secureTextEntry={!passwordShown}
+            className="border-[#4B0082] border-2 rounded-lg bg-[#F8F7FE] text-2xl pl-3 text-black"
             onChangeText={(text) => setUserIn({ ...userIn, password: text })}
           />
+          <TouchableOpacity onPress={() => setPasswordShown(!passwordShown)}>
+            <Text
+              className="text-[#4B0082] font-bold text-lg mt-3"
+              style={{ fontFamily: "InriaSans-Regular" }}
+            >
+              {passwordShown ? "Hide" : "Show"}
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View className="m-10">
+        <View className="m-10 mt-5">
           <TouchableOpacity
             onPress={login}
             className="bg-[#E4007C] rounded-lg py-3 mb-5"
