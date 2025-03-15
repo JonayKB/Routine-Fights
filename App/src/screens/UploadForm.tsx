64c61b50a7399572ab8025209928cb68fb6777de
rabgonzalez <rubalba.rag@gmail.com>
2025-03-15 21:26:44 +0100

    feat(ImageCrop): function for editing images done

--- App/src/screens/UploadForm.tsx
+++ App/src/screens/UploadForm.tsx
@@ -1,20 +1,17 @@
-import { View, Image } from "react-native";
-import React, { useEffect, useState } from "react";
+import { Image, View } from "react-native";
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { UploadStackProps } from "../navigation/UploadStackNavigation";
 
 type Props = NativeStackScreenProps<UploadStackProps, "UploadForm">;
 
 const UploadForm = ({ navigation, route }: Props) => {
-  const [image, setImage] = useState<string>("");
-
-  useEffect(() => {
-    setImage(route.params.image);
-  }, [route.params.image]);
-
   return (
     <View>
-      <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      <Image
+        source={{ uri: `file://${route.params.uri}` }}
+        style={{ width: 281, height: 500 }}
+      />
     </View>
   );
 };
