b5a23ab9c74450e6397415895ce06e40359e5063
JonayKB <jonaykb@gmail.com>
2025-04-19 22:08:31 +0200

    test(Graph): Created tests

--- API/src/main/java/es/iespuertodelacruz/routinefights/shared/dto/ChartData.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/shared/dto/ChartData.java
@@ -1,5 +1,6 @@
 package es.iespuertodelacruz.routinefights.shared.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChartData {
@@ -7,6 +8,8 @@ public class ChartData {
     private List<Long> data;
 
     public ChartData() {
+        this.labels = new ArrayList<>();
+        this.data = new ArrayList<>();
     }
     
     public ChartData(List<String> labels, List<Long> data) {
@@ -28,6 +31,27 @@ public class ChartData {
 
     public void setData(List<Long> data) {
         this.data = data;
+    }
+
+    public void addLabel(String label) {
+        this.labels.add(label);
+    }
+    public void addData(Long data) {
+        this.data.add(data);
+    }
+
+    @Override
+    public String toString() {
+        return "ChartData [labels=" + labels + ", data=" + data + "]";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        ChartData other = (ChartData) obj;
+        return labels.equals(other.labels) && data.equals(other.data);
     }
 
 }
