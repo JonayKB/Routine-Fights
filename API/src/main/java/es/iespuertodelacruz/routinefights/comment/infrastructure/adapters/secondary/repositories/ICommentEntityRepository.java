8ddec259d4fea0d9a2573852a6e0f5b1775718ee
JonayKB <jonaykb@gmail.com>
2025-04-30 09:55:05 +0200

    test(Comment): Added Comment Domain test

--- API/src/main/java/es/iespuertodelacruz/routinefights/comment/infrastructure/adapters/secondary/repositories/ICommentEntityRepository.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/comment/infrastructure/adapters/secondary/repositories/ICommentEntityRepository.java
@@ -1,11 +1,8 @@
 package es.iespuertodelacruz.routinefights.comment.infrastructure.adapters.secondary.repositories;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.neo4j.repository.query.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import es.iespuertodelacruz.routinefights.comment.infrastructure.adapters.secondary.entities.CommentEntity;
