97ecbe273be55471135fc4eefa947cd5f49c192e
JonayKB <jonaykb@gmail.com>
2025-05-16 21:19:46 +0200

    test(Comment): added comment vontoller v2 tests

--- API/src/test/java/es/iespuertodelacruz/routinefights/comment/domain/services/CommentServiceTest.java
+++ API/src/test/java/es/iespuertodelacruz/routinefights/comment/domain/services/CommentServiceTest.java
@@ -3,7 +3,6 @@ package es.iespuertodelacruz.routinefights.comment.domain.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
