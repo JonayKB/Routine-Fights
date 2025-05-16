d459cf864eb13a8c2af56f8b35f0f5f14868f284
JonayKB <jonaykb@gmail.com>
2025-05-16 20:12:45 +0200

    fix(Comment): Dont lose information QUICK FIX

--- API/src/main/java/es/iespuertodelacruz/routinefights/comment/infrastructure/adapters/secondary/services/CommentEntityService.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/comment/infrastructure/adapters/secondary/services/CommentEntityService.java
@@ -6,23 +6,32 @@ import org.springframework.stereotype.Service;
 
 import es.iespuertodelacruz.routinefights.comment.domain.Comment;
 import es.iespuertodelacruz.routinefights.comment.domain.ports.secondary.ICommentRepository;
+import es.iespuertodelacruz.routinefights.comment.infrastructure.adapters.secondary.entities.CommentEntity;
 import es.iespuertodelacruz.routinefights.comment.infrastructure.adapters.secondary.mappers.CommentEntityMapper;
 import es.iespuertodelacruz.routinefights.comment.infrastructure.adapters.secondary.repositories.ICommentEntityRepository;
+import es.iespuertodelacruz.routinefights.user.domain.User;
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.entities.UserEntity;
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.repositories.IUserEntityRepository;
 
 @Service
 public class CommentEntityService implements ICommentRepository {
 
     private ICommentEntityRepository commentRepository;
     private CommentEntityMapper commentEntityMapper;
+    private IUserEntityRepository userRepository;
 
-    public CommentEntityService(ICommentEntityRepository commentRepository, CommentEntityMapper commentEntityMapper) {
+    public CommentEntityService(ICommentEntityRepository commentRepository, CommentEntityMapper commentEntityMapper,
+            IUserEntityRepository userRepository) {
+        this.userRepository = userRepository;
         this.commentEntityMapper = commentEntityMapper;
         this.commentRepository = commentRepository;
     }
 
     @Override
     public Comment comment(Comment comment) {
-        return commentEntityMapper.toDomain(commentRepository.save(commentEntityMapper.toEntity(comment)));
+        CommentEntity commentEntity = commentEntityMapper.toEntity(comment);
+
+        return commentEntityMapper.toDomain(commentRepository.save(commentEntity));
     }
 
     @Override
