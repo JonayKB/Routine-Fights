2da2930c6e95eb21ae7aabe39d1cbf7891c71c91
rabgonzalez <rubalba.rag@gmail.com>
2025-05-18 23:04:33 +0100

    style(Login, Register): login and register screen's style improved

--- App/src/components/BadgeInfo.tsx
+++ App/src/components/BadgeInfo.tsx
@@ -1,14 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from "react-native";
+import React from "react";
+import { Badge } from "../utils/Badge";
+import Picture from "./Picture";
 
-type Props = {}
+type Props = {
+  item: Badge;
+};
 
-const BadgeInfo = (props: Props) => {
+const BadgeInfo = ({ item }: Props) => {
   return (
-    <View>
-      <Text>BadgeInfo</Text>
+    <View className="flex-row items-center justify-evenly p-6 px-8 bg-white rounded-xl shadow-md">
+      <Picture image={item.image} size={100} style="rounded-full mt-4" />
+      <View className="ml-8">
+        <Text className="text-2xl font-bold text-[#333333] mb-2">
+          {item.communityEvent?.name}
+        </Text>
+        <Text className="text-[#333333]">Level: {item.level}</Text>
+        <Text className="text-[#333333]">
+          Points Needed: {item.communityEvent?.totalRequired}
+        </Text>
+        <Text className="text-[#333333]">
+          End Date: {item.communityEvent?.finishDate.slice(0, 10)}
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default BadgeInfo
+export default BadgeInfo;
--- App/src/utils/Badge.ts
+++ App/src/utils/Badge.ts
@@ -1,9 +1,8 @@
+import { Event } from "./Event";
+
 export type Badge = {
   image: string;
   level: number;
   id: string;
-  communityEvent: {
-    id: string;
-    name: string;
-  };
+  communityEvent: Event;
 };
