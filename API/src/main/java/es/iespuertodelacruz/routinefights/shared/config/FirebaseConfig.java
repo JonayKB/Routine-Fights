ec986147d743b884ba85d1fd25b89665dfc75f63
Jonay Contreras <88539815+JonayKB@users.noreply.github.com>
2025-10-29 20:33:44 +0000

    Add import for List in FirebaseConfig.java

--- API/src/main/java/es/iespuertodelacruz/routinefights/shared/config/FirebaseConfig.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/shared/config/FirebaseConfig.java
@@ -3,6 +3,8 @@ package es.iespuertodelacruz.routinefights.shared.config;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
