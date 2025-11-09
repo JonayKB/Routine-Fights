7816697d951542cd240566e615001db538f9502f
JonayKB <jonaykb@gmail.com>
2025-11-09 23:32:00 +0000

    Removed infinite loop

--- API/src/main/java/es/iespuertodelacruz/routinefights/device_token/infrastructure/adapters/secondary/mappers/IDeviceTokenEntityMapper.java
+++ API/src/main/java/es/iespuertodelacruz/routinefights/device_token/infrastructure/adapters/secondary/mappers/IDeviceTokenEntityMapper.java
@@ -2,14 +2,32 @@ package es.iespuertodelacruz.routinefights.device_token.infrastructure.adapters.
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import es.iespuertodelacruz.routinefights.device_token.domain.DeviceToken;
 import es.iespuertodelacruz.routinefights.device_token.infrastructure.adapters.secondary.entities.DeviceTokenEntity;
+import es.iespuertodelacruz.routinefights.user.domain.User;
+import es.iespuertodelacruz.routinefights.user.infrastructure.adapters.secondary.entities.UserEntity;
 
 @Mapper(componentModel = "spring")
 public interface IDeviceTokenEntityMapper {
     DeviceToken toDomain(DeviceTokenEntity deviceTokenEntity);
 
-    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "user", source = "user", qualifiedByName = "userDomainToEntity")
     DeviceTokenEntity toEntity(DeviceToken deviceToken);
+
+    @Named("userDomainToEntity")
+    default UserEntity userDomainToEntity(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(user.getId());
+        userEntity.setUsername(user.getUsername());
+        userEntity.setEmail(user.getEmail());
+        userEntity.setPassword(user.getPassword());
+        
+        return userEntity;
+    }
 }
